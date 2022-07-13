762a289b5c831e9637d4cf337aace86d61a59d47
SHIQIU SUn <shiqiusun@MacBook-Air.local>
2022-07-13 12:13:44 -0700

    Assignment 7

---
+++ sun.shiqiu/about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+	
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">		
+			<h2>About</h2>
+		<p class="p1">An artisanal approach, unrivaled attention to detail, and universally flattering carefree forms have won the Spanish designer, Rosa Esteva, a faithful fashion following which seeks the brand’s feminine relaxed silhouettes for day and evening.</p>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ sun.shiqiu/parts/navbar.php
@@ -0,0 +1,16 @@
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+		<h1>Nana's Fashion Store</h1>
+		</div>
+	<div class="flex-stretch"></div>
+	 <nav class="flex-none nav">
+      		<ul class="container display-flex">
+      			<li><a href="index.html">Home</a></li>
+      			<li><a href="product_list.php">Collection</a></li>
+      			<li><a href="about.php">About</a></li>
+      			<li><a href="cart.php">Cart</a></li>
+      		</ul>
+      	</nav> 
+      	</div>	
+	</header>
