5a7c31958121bff48f1cf24510b3701a4eb86113
OwenZhao1 <107844835+OwenZhao1@users.noreply.github.com>
2022-08-11 09:06:48 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,22 @@
+<?php include_once "lib/php/functions.php"; ?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About Page</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>About Us</h2>
+			<p>This summer let beer and drink with you！</p>
+			<p>Let's cheers!</p>
+		</div>
+	</div>
+	
+</body>
+</html>
